c47aa2d9a9d9eecafc99c174e42e66e9668738bc
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-08 20:04:32 -0300

    Iniciando o servidor.

--- server.py
+++ server.py
@@ -1,2 +1,28 @@
-import socket
-import ssl
+import socket, ssl, sys
+
+port = int(sys.argv[1])
+tcp = True
+if len(sys.argv) > 2:
+    if sys.argv[2] == 'udp':
+        tcp = False
+    elif sys.argv[2] == 'tcp':
+        tcp = True
+    else:
+        raise Exception("Unknown method: " + sys.argv[2])
+
+if not tcp:
+    raise Exception("UDP NYI")
+    
+    
+users = {}
+    
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("0.0.0.0", port))
+sock.listen(0)
+
+for _ in range(2): # por enquanto, para poder matar
+    new_client, client_address = sock.accept()
+    print(client_address)
+    new_client.close()
+
+sock.close()
