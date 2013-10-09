2805f767229b70ff2fb10b6ba0d4ddd95c79a937
Gustavo Teixeira da Cunha Coelho <gustavotdaccoelho@gmail.com>
2013-10-09 16:09:16 -0300

    THREADS STUFF

--- server.py
+++ server.py
@@ -1,4 +1,39 @@
-import socket, ssl, sys
+from socket import *
+import socket, ssl, sys, threading
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+        print "[+] New thread started for "+str(ip)+":"+str(port)+"\n"
+
+    def run(self):
+        try:
+            data = self.socket.recv(2048)
+        except socket.timeout:
+            return
+        if len(data) == 0:
+            return
+        for command in filter(None, data.splitlines()):
+            if command.split(' ', 1)[0].upper() == 'LOGIN':
+                try_username = command.split(' ', 1)[1]
+                if try_username in users:
+                    self.socket.send("USER_ALREADY_EXISTS\n")
+                else:
+                    username = try_username
+                    users[username] = True
+                    self.socket.send("WELCOME\n")
+                # STUFF TO DO ON LOGIN
+            elif command.upper() == "LOGOUT":
+                client_running = False
+                self.socket.close()
+                return
+            else: # command == "HEARTBEAT"
+                pass
+
 
 port = int(sys.argv[1])
 tcp = True
@@ -21,39 +56,26 @@ users = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("0.0.0.0", port))
 sock.listen(0)
+threads = []
 
-for _ in range(2): # por enquanto, para poder matar
-    print("== WATING FOR CLIENT!")
-    new_client, client_address = sock.accept()
-    new_client.settimeout(HEARTBEAT_INTERVAL)
+while 1:#for _ in range(2): # por enquanto, para poder matar
+    print("== WAITING FOR CLIENT! ==")
+    (clientsock, (ip, port)) = sock.accept()
+    clientsock.settimeout(HEARTBEAT_INTERVAL)
+    newthread = ClientThread(ip, port, clientsock)
+    newthread.start()
+    threads.append(newthread)
     
     username = None
     client_running = True
     
-    while client_running:
-        try:
-            data = new_client.recv(2048)
-        except socket.timeout:
-            break
-        if len(data) == 0:
-            break
-        for command in filter(None, data.splitlines()):
-            if command.split(' ', 1)[0].upper() == 'LOGIN':
-                try_username = command.split(' ', 1)[1]
-                if try_username in users:
-                    new_client.send("USER_ALREADY_EXISTS\n")
-                else:
-                    username = try_username
-                    users[username] = True
-                    new_client.send("WELCOME\n")
-                # STUFF TO DO ON LOGIN
-            elif command.upper() == "LOGOUT":
-                client_running = False
-                break
-            else: # command == "HEARTBEAT"
-                pass
+    '''while client_running:
+        
     if username:
         del users[username]
-    new_client.close()
+    new_client.close()'''
+
+for thread in threads:
+    thread.join()
 
 sock.close()
